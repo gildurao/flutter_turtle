a8581e3d9efe19248f61211c267ad50c1ce537a5
Gil Durão <43495596+gildurao@users.noreply.github.com>
2020-08-30 22:31:47 +0100

    Testing some more

--- example/lib/pages/lsystem.dart
+++ example/lib/pages/lsystem.dart
@@ -37,7 +37,7 @@ class _LSystemPageState extends State<LSystemPage> {
 
   @override
   Widget build(BuildContext context) {
-    dragon.generate(6);
+    dragon.generate(2);
     return Scaffold(
       body: InteractiveViewer(
         child: Column(
